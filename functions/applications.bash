e6a7b7c8664b15eeadbba90a4b1a87a699b45c52
Jason <jason@casjay.net>
2021-01-08 04:09:12 -0500

    🏠🐜❗ Minor Fixes ❗🐜🏠

--- functions/applications.bash
+++ functions/applications.bash
@@ -470,8 +470,8 @@ get_app_info() {
   if [ -f "$FILE" ]; then
     echo ""
     cat "$FILE" | grep "# @" | grep " : " >/dev/null 2>&1 &&
-      cat "$FILE" | grep "# @" | grep " : " &&
-      printf_green "Version: $(cat $FILE | grep "##@Version" | grep " : " | printf_newline "3")" ||
+      cat "$FILE" | grep "# @" | grep " : " | printf_newline "3" &&
+      printf_green "Version: $(cat $FILE | grep "##@Version" | sed 's/##@Version.*: //g')" ||
       printf_red "File was found, however, No information was provided"
     echo ""
   else
