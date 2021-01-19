6a62b1ff4cf0abbf7d2f952da6ff9e9741dee289
Jason <jason@casjay.net>
2021-01-19 00:23:24 -0500

    🏠🐜❗ Minor Fixes ❗🐜🏠

--- helpers/man/cheat.sh
+++ helpers/man/cheat.sh
@@ -8,11 +8,9 @@ printf_help() {
   printf_color "\t\t$msg\n" "$color"
 }
 #----------------
-printf_help "4" "Usage:"
-printf_help "4" "QUERY                               process QUERY and exit"
-printf_help "4" "--help                              show this help"
-printf_help "4" "--shell [LANG]                      shell mode (open LANG if specified)"
-printf_help "4" "--standalone-install [DIR|help]     install cheat.sh in the standalone mode (by default, into ~/.config/cheat.sh)"
-printf_help "4" "--mode [auto|lite]                  set (or display) mode of operation"
-printf_help "4" "                                    * auto - prefer the local installation"
-printf_help "4" "                                    * lite - use the cheat sheet server"
+printf_help "2" "Lookup utitilies"
+printf_help "4" "cheat.sh QUERY                            |  process QUERY and exit"
+printf_help "4" "cheat.sh --help                           |  show this help"
+printf_help "4" "cheat.sh --shell [LANG]                   |  shell mode (open LANG if specified)"
+printf_help "4" "cheat.sh --standalone-install [DIR|help]  |  install cheat.sh in the standalone mode"
+printf_help "4" "cheat.sh --mode [auto|lite]               |  set (or display) mode of operation"
