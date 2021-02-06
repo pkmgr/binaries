9a2bd68616c665be3fe00c4add5863c4c97ef300
Jason <jason@casjay.net>
2021-02-06 00:43:34 -0500

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- functions/testing.bash
+++ functions/testing.bash
@@ -36,9 +36,9 @@ if ! command -v "git" >/dev/null 2>&1; then
 fi
 
 ###################### devnull handling ######################
-__devnull() { "$@" >/dev/null 2>&1; }
-__devnull1() { "$@" 1>/dev/null >&0; }
-__devnull2() { "$@" 2>/dev/null; }
+__devnull() { eval "$@" >/dev/null 2>&1; }
+__devnull1() { eval "$@" 1>/dev/null >&0; }
+__devnull2() { eval "$@" 2>/dev/null; }
 ###################### error handling ######################
 #err "commands"
 __err() {
