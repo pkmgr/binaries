54abec6b6af2060cdbfa21b6f3baa0c433c95ab8
Jason <jason@casjay.net>
2021-03-13 01:51:17 -0500

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- templates/scripts/shell/user.bash
+++ templates/scripts/shell/user.bash
@@ -12,8 +12,8 @@ SRC_DIR="${BASH_SOURCE%/*}"
 ##@Version       : REPLACE_VERSION
 # @Author        : REPLACE_AUTHOR
 # @Contact       : REPLACE_EMAIL
-# @License       : REPLACE_LICENSE
-# @ReadME        : REPLACE_README
+# @License       : WTFPL
+# @ReadME        : REPLACE_FILENAME --help
 # @Copyright     : REPLACE_COPYRIGHT
 # @Created       : REPLACE_DATE
 # @File          : REPLACE_FILENAME
@@ -25,16 +25,19 @@ SRC_DIR="${BASH_SOURCE%/*}"
 # helper function
 __version() { app_version; }
 __help() {
-  app_help "Usage:  REPLACE_FILENAME |  REPLACE_FILENAME "
+  app_help "Usage: REPLACE_FILENAME  |  REPLACE_FILENAME "
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Main function
 main() {
-  [ -f "$SRC_DIR/functions.bash" ] && local DIR="$SRC_DIR" || local DIR="$HOME/.local/bin"
-  if [[ -f "$DIR/functions.bash" ]]; then
-    . "$DIR/functions.bash"
+  if [ -f "$SRC_DIR/functions.bash" ]; then
+    FUNCTIONS_DIR="$SRC_DIR"
+    . "$FUNCTIONS_DIR/functions.bash"
+  elif [ -f "$HOME/.local/bin/functions.bash" ]; then
+    FUNCTIONS_DIR="$HOME/.local/bin"
+    . "$FUNCTIONS_DIR/functions.bash"
   else
-    printf "\t\t\\033[0;31m%s \033[0m\n" "Couldn't source the functions file from $DIR"
+    printf "\t\t\033[0;31m%s \033[0m\n" "Couldn't source the functions file from $FUNCTIONS_DIR"
     return 1
   fi
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -42,10 +45,10 @@ main() {
   [ "$1" = "--help" ] && __help
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Required app check
-  cmd_exists --error $APPNAME || exit 1
+  cmd_exists --error REPLACE_FILENAME || exit 1
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  !
-  return "${exitCode:-$?}"
+  #
+  return "${exitCode:-0}"
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # execute function
